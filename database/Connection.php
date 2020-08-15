2f13f73aad96e1be49ee1a1d32d967cf026e1af0
Sayedfahim <69226624+Sayedfahim@users.noreply.github.com>
2020-08-16 05:17:23 +0600

    Add files via upload

---
+++ database/Connection.php
@@ -0,0 +1,19 @@
+<?php
+$servername = 'localhost';
+$Username = 'root';
+$password = '';
+
+
+// connecting to database
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=inventory", $Username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+}
+catch (\Exception $e) {
+
+$error_message = $e->getMessage();
+}
+
+
+?>
